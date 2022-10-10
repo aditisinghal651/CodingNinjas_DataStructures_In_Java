f5ccb86a427efdf345159a8aca3774966714e743
aditisinghal651 <iamaditisinghal25@gmail.com>
2022-10-10 14:59:46 +0530

    Initial Commit

--- DataStructures/arrayList/RemoveDuplicates.java
+++ DataStructures/arrayList/RemoveDuplicates.java
@@ -1,10 +1,10 @@
-package com.codingninjas.arrayList;
+package DataStructures.arrayList;
 
 import java.util.ArrayList;
 
 public class RemoveDuplicates {
 
-    public static ArrayList<Integer> RemoveConsecutiveDupli(int arr[]){
+    public static ArrayList<Integer> RemoveConsecutiveDuplicate(int arr[]){
         ArrayList<Integer> Result = new ArrayList<>();
         Result.add(arr[0]);
         for(int i = 1; i < arr.length; i++){
@@ -17,7 +17,7 @@ public class RemoveDuplicates {
 
     public static void main(String args[]){
         int arr[] = {10, 10, 20, 20, 30, 30, 30, 40, 10};
-        ArrayList<Integer> result = RemoveConsecutiveDupli(arr);
+        ArrayList<Integer> result = RemoveConsecutiveDuplicate(arr);
         for(int i : result){
             System.out.println(i);
         }
